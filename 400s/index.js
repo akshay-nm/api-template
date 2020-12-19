bd5c327ac673cd0a3624227f18cae8a2f2769613
Akshay Kumar <work.akshay.kumar@gmail.com>
2020-12-20 01:13:54 +0530

    Fix type

--- 400s/index.js
+++ 400s/index.js
@@ -1,7 +1,4 @@
 module.exports = {
   sessions: require('./session.400s'),
   users: require('./user.400s'),
-  driverConfigurations: require('./driver-configuration.400s'),
-  farmerConfigurations: require('./farmer-configuration.400s'),
-  storageConfigurations: require('./storage-configuration.400s'),
 }
