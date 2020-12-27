a93281beae349fc648fd355b5e98d77c29abbaf7
Akshay Kumar <work.akshay.kumar@gmail.com>
2020-12-28 00:46:25 +0530

    Add favicon, fix header in redoc html

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
+const path = require('path')
 const router = require('express').Router()
 
 router.use('/api', require('./api'))
 router.use('/docs', require('./docs'))
+router.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'favicon.ico'))
+})
 
 module.exports = router
